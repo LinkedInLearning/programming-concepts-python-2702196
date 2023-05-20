39f848379f44e9f1c1386341aed435342d4917eb
Barron Stone <7518212+barronstone@users.noreply.github.com>
2023-05-20 23:05:14 +0000

    initial test commit

---
+++ examples/05_01_lists/lists_commands.py
@@ -0,0 +1,32 @@
+""" Parking Cars in a List """
+
+# create the initial list of cars
+row = ['Ford', 'Audi', 'BMW', 'Lexus']
+
+# park a Mercedes at the end of the row
+row.append('Mercedes')
+print(row)
+print(row[4])
+
+# swap a BMW at index 2 for a Jeep
+row[2] = 'Jeep'
+print(row)
+
+# park a Honda at the end of the row
+row.append('Honda')
+print(row)
+print(row[4])
+
+# park a Kia at the front of the row
+row.insert(0, 'Kia')
+print(row)
+print(row[4])
+
+# find the Mercedes and leave the list
+print(row.index('Mercedes'))
+print(row.pop(5))
+print(row)
+
+# find and remove a Lexus
+row.remove('Lexus')
+print(row)
